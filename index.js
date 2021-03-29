abab0c2560674bc8e955f31f3f9b33cf04bd5a1d
Jacob Marthaller <marthaller.jr@gmail.com>
2021-03-29 06:44:03 -0700

    working solution

---
+++ index.js
@@ -0,0 +1,13 @@
+function countChar(string, ch) {
+    let counted = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] == ch) {
+        counted += 1;
+      }
+    }
+    return counted;
+  }
+  
+  function countBs(string) {
+    return countChar(string, "B");
+  }
